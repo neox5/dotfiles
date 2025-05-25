5eb5b231a289786657fef96dab6b072267b3e11d
neox5 <neox5@orion.localdomain>
2025-05-25 21:46:52 +0200

    removes old nvim folder

--- nvim/.config/nvim/lua/core/keymaps.lua
+++
@@ -1,6 +0,0 @@
-local keymap = vim.keymap.set
-local opts = { noremap = true, silent = true }
-
--- netrw
-keymap("n", "<leader>pe", vim.cmd.Ex, opts) -- po..."project open"
-
--- nvim/.config/nvim/lua/plugins/ansible.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "pearofducks/ansible-vim",
-  ft = { "yaml.ansible" },
-  config = function()
-    -- Optional: Configure ansible-vim settings here
-    vim.g.ansible_unindent_after_newline = 1
-    vim.g.ansible_extra_keywords_highlight = 1
-  end
-}
