058fa7b19f68e57e8023112b901aa2b6f6b5abf2
neox5 <neox5@orion.localdomain>
2025-06-04 07:11:50 +0200

    adds pyenv environment to zsh config

--- config/zsh/environment.zsh
+++ config/zsh/environment.zsh
@@ -5,3 +5,8 @@ export PATH="$HOME/bin:$PATH"
 export GOPATH="$HOME/go"
 export PATH="$GOPATH/bin:$PATH"
 
+# pyenv setup
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
