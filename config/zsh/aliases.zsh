cda4dbac8ed497fab718b3b5fa37a9fae27b5121
neox5 <faustmannchr@gmail.com>
2026-05-23 07:49:01 +0200

    add alias switch for open alias

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -1,7 +1,11 @@
 alias ll="ls -lah"
 alias dotfiles="cd $DOTFILES"
 alias nv="nvim"
-alias open="dolphin"
+if command -v nautilus &>/dev/null; then
+  alias open="nautilus"
+elif command -v dolphin &>/dev/null; then
+  alias open="dolphin"
+fi
 alias clip="xclip -selection clipboard"
 alias py="python"
 alias ccli="cardano-cli"
